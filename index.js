88f2689ac51bbfaa7b4be7224aac6ce4f4e67113
Alex Emmanuel <117074890+alexlandjou@users.noreply.github.com>
2022-11-09 17:21:36 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,95 @@
+// // // // // let chiffre = 47;
+
+// // // // // let nouvelleChaine = `le chiffre attendue est : ${chiffre} degrès`;
+
+// // // // // console.log(nouvelleChaine);
+
+
+// // // // let string = "je suis une chaine";
+// // // // let number = 24;
+// // // // let boolean = true;
+// // // // let array = ["je", "suis", 24, false];
+// // // // let objet = {
+// // // //     prenom: "audray",
+// // // //     age: 34,
+// // // //     ville: "paris",
+
+// // // // };
+
+// // // // let arbre = null;
+// // // // console.log(typeof arbre);
+
+// // // // let x = 6
+// // // // let y = "5"
+
+// // // // if(x === y) {
+// // // //     console.log("true!");
+// // // // } else if (x == y) {
+// // // //     console.log("x et y égaux en valeur");
+// // // // } else {
+// // // //     console.log(x + " et " + y + " ne sont pas pas du tout égaux en valeur");
+// // // //     console.log(`${x} et ${y} ne sont pas pas du tout égaux en valeur`);
+// // // // }
+
+// // // // let x = 6
+// // // // let y = "5"
+
+// // // // x == y ? console.log("true !") : console.log("false");
+ 
+
+// // // function faireQuelqueChose() {
+// // //     console.log("je fais un truc !");
+// // //     console.log("trop cool");
+// // // }
+
+// // // const faireUneTache = (tache) => {
+// // //     console.log("je fais : "+ tache);
+// // // }
+// // // faireUneTache("les course");
+// // // faireUneTache("le menage");
+
+// // // (() => {
+// // //     console.log();
+// // // })
+
+
+  
+// //     let nom = "Alex";
+// //    let mentale = 10;
+
+// // let personnage = `${nom} a une santé mentale de ${mentale}`;
+// // console.log(personnage);
+
+// //  let array = ["Anissa-Wejdene", "_Amour-Niska", "_Jaloux-Dadju",
+// //   "_Tomber-Ronissia", "_Soza-Tiakola"]
+
+// // console.log(array[0] + array[1] + array[2] + array[3] + array[4]);
+
+// // let objet = "Radio";
+// // let feux = 30;
+// // let changment = null
+// // let destination = "arrivée"
+
+
+// let name = "jason";
+// let point= 100;
+
+// let array = ["nerd", "sportif", "blond"];
+
+// const names = [
+//     "john",
+//     "eduardo",
+//     "eric",
+//     "jeanne",
+//     "elise",
+// ]
+// let survivor = new survivor(
+//     names[Math.floor (math.radam()=names.length)],
+//     carac[Math.floor(math.ramdom() = carac.length)]
+// )
+// console.log(survivor1)
+
+// while(point>o,names=5) {
+//     console.log(survivor)
+
+// }
